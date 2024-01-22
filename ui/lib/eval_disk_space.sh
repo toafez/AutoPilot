1aa3a8e990d6f92d5431d226af37eb05ff0dfe9d
luddinho <info@luddi.org>
2024-01-22 16:39:32 +0100

    renamed local references and added comments

--- ui/lib/eval_disk_space.sh
+++ ui/lib/eval_disk_space.sh
@@ -2,18 +2,19 @@
 
 # Read disk size in bytes via `df` command of given mountpoint
 function evalDiskSize (){
-    local -n ref_mountpoint=$1
-    local -n ref_ext_disk_size=$2
-    local -n ref_ext_disk_used=$3
-    local -n ref_ext_disk_available=$4
-    local -n ref_ext_disk_used_percent=$5
-    local -n ref_ext_disk_mountpoint=$6
+    local -n ref_mountpoint=$1  # Mountpoint to evaluate
+
+    local -n ref_size=$2        # size of device
+    local -n ref_used=$3        # Used size of device
+    local -n ref_available=$4   # Available size of device
+    local -n ref_usage=$5       # Percentage of used size
+    local -n ref_mounted_on=$6  # Mounted on
 
     local df=$(df -B1 "${ref_mountpoint}")
     df=$(echo "${df}" | sed -e 's/%//g' | awk 'NR > 1 {print $2 " " $3 " " $4 " " $5 " " $6}')
-    ref_ext_disk_size=$(echo "${df}" | awk '{print $1}')
-    ref_ext_disk_used=$(echo "${df}" | awk '{print $2}')
-    ref_ext_disk_available=$(echo "${df}" | awk '{print $3}')
-    ref_ext_disk_used_percent=$(echo "${df}" | awk '{print $4}')
-    ref_ext_disk_mountpoint=$(echo "$df" | awk '{print $5}')
+    ref_size=$(echo "${df}" | awk '{print $1}')
+    ref_used=$(echo "${df}" | awk '{print $2}')
+    ref_available=$(echo "${df}" | awk '{print $3}')
+    ref_usage=$(echo "${df}" | awk '{print $4}')
+    ref_mounted_on=$(echo "$df" | awk '{print $5}')
 }
