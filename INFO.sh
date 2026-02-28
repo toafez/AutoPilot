40237a1958cf4ce48903c39e2ee64684e7b10a4d
Tommes <101346537+toafez@users.noreply.github.com>
2026-02-28 09:12:17 +0100

    Add distributor URL and help URL to INFO.sh

--- INFO.sh
+++ INFO.sh
@@ -20,9 +20,10 @@ package_icon="PACKAGE_ICON.PNG"
 package_icon_256="PACKAGE_ICON_256.PNG"
 dsmappname="SYNO.SDS.AutoPilot.Application"
 maintainer="Tommes"
+distributor_url="https://github.com/toafez/AutoPilot/releases"
 description="AutoPilot for external drives enables shell script instructions to be executed automatically after connecting an external data carrier. After execution, the external data medium can be automatically ejected again if desired."
 description_enu="AutoPilot for external drives enables shell script instructions to be executed automatically after connecting an external data carrier. After execution, the external data medium can be automatically ejected again if desired."
 description_ger="AutoPilot für externe Datenträger ermöglicht das Ausführen von Shell-Script Anweisungen, die nach dem Anschluss eines externen Datenträgers automatisch ausgeführt werden. Nach der Ausführung kann der externe Datenträger auf Wunsch wieder automatisch ausgeworfen werden."
-helpurl=""
+helpurl="https://www.synology-forum.de/threads/autopilot-fuer-externe-datentraeger.128487"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
